e418cea9bab0a6a8c062dac7f09fe9ce8bb48b5a
Barak Michener <me@barakmich.com>
2010-03-05 14:44:06 -0800

    Initial Load
    --HG--
    rename : README => README.mkd

---
+++ datagrab.py
@@ -0,0 +1,17 @@
+import freebase
+import json
+import sys
+from pprint import pprint as pp
+
+f = open(sys.argv[1], "r")
+q = json.load(f)
+f.close()
+it = freebase.mqlreaditer(q)
+print "["
+for i in range(int(sys.argv[2])):
+	out = it.next()
+	print json.dumps(out, indent=2)
+	if i != int(sys.argv[2]) - 1:
+		print ","
+	
+print "]"
